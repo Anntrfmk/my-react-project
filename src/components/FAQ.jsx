a08d83ccc33ec7767feface100bf57b6d25e9313
Anntrfmk <nntrfmk@gmail.com>
2026-09-07 10:19:40 +0300

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -17,13 +17,14 @@ function FAQ (){
             <h1 className="text-3xl sm:text-2xl font-bold text-pink-400 ">Часті запитання</h1>
            <div>
             {FAQdata.map((item)=>(
-                <div key={item.id} className="grid mt-5 p-4 max-h-12 max-w-4xl bg-slate-50 grid-cols-2 sm:grid-cols-3 mx-auto shadow-sm border border-slate-100 ">
-                    <h3 className="">{item.question}</h3>
-                    <button onClick={() => click(item.answer)} className=""><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <div key={item.id} className="grid mt-5 p-4 max-h-18 max-w-4xl bg-slate-50 grid-cols-2 sm:grid-cols-3 mx-auto shadow-sm border border-slate-100 text-center justify-center ">
+                    <h3 className="mb-5 text-center text-sm">{item.question}</h3>
+                    <div className="items-end max-w-4xl">
+                    <button onClick={() => click(item.answer)} className="mb-2 text-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 </svg>
 </button>
-                    
+                    </div>
                 </div>
             ))}
            </div>
